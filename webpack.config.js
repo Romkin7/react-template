fa2ddbbbdc03668ed38f82a4131f7d8a97b1ae90
Roman Tuomisto <romantuomisto@gmail.com>
2022-11-03 09:21:19 +0200

    adds typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,39 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     // Where files should be sent once they are bundled
+    entry: './src/index.tsx',
     output: {
         path: path.join(__dirname, '/build'),
         filename: 'index.bundle.js',
     },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     // webpack 5 comes with devServer which loads in development mode
     devServer: {
         port: 3000,
-        watchContentBase: true,
+        hot: true,
     },
     // Rules of how webpack will take our files, complie & bundle them for the browser
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: /nodeModules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['ts-loader'],
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'css-modules-typescript-loader',
+                ],
             },
         ],
     },
